bf30cee1a8f4d2a6821c25120ee56f35886f086d
ShivaBhattacharjee <shivaxwarrior@gmail.com>
2024-06-10 00:38:59 +0530

    idk

--- src/app/identity/page.tsx
+++ src/app/identity/page.tsx
@@ -19,9 +19,9 @@ const Page = () => {
 
             try {
                 jwt.decode(token);
-                // setCookie("token", token , {
-                //     maxAge: 60 * 60 * 24 * 7, 
-                // })
+                setCookie("token", token , {
+                    maxAge: 60 * 60 * 24 * 7, 
+                })
                 router.push("/");
             } catch (e) {
                 console.error(e);
